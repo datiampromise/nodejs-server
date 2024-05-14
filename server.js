9bdd7d812876a19a5391bbc46534e020e2e772b1
datiampromise <promiseuzochukw085@gmail.com>
2024-05-14 02:19:06 +0100

    added CORS

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const http = require('http');
 
-
 http.createServer((req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
     res.writeHead(200, { 'Content-Type': 'text/html' });
 
     if (req.url === '/' && req.method === 'GET') {
